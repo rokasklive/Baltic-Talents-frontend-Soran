73bc5e1c12a1b8fc607ea352cfce594d096be2e9
Rokas <rokask@live.com>
2018-07-16 08:18:16 +0300

    Main finish

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,6 +6,29 @@ $(document).ready(function(){
       autoplay: true,
       autoplaySpeed: 4000,
       prevArrow: $('.next'),
-      nextArrow: $('.prev')
+      nextArrow: $('.prev'),
+      responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
   });
 });
